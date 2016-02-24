c320be3793bac128ed7ed8aad386d5836760f81d
Gabriella Lopes <gabriellablopes@Gabriella-Lopes.local>
2016-02-24 15:31:34 -0300

    Segundo commit

--- projetoBobao/projetoBobao/ViewController.swift
+++ projetoBobao/projetoBobao/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var resultado: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func apertouSim(sender: AnyObject) {
+        resultado.text = "Errou!"
+    }
+    
+    
+    @IBAction func apertouNao(sender: AnyObject) {
+        resultado.text = "Acertou!"
+    }
+    
+    
 
 }
 
