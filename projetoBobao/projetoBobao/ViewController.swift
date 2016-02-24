3dbb424e7e12e074dca19d31d550421445a0b680
Gabriella Lopes <gabriellablopes@Gabriella-Lopes.local>
2016-02-24 15:52:26 -0300

    mudei o texto da acao

--- projetoBobao/projetoBobao/ViewController.swift
+++ projetoBobao/projetoBobao/ViewController.swift
@@ -26,12 +26,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func apertouSim(sender: AnyObject) {
-        resultado.text = "Errou!"
+        resultado.text = "Errou tudo!"
     }
     
     
     @IBAction func apertouNao(sender: AnyObject) {
-        resultado.text = "Acertou!"
+        resultado.text = "Acertou tudo!"
     }
     
     
